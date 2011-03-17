035ae4fdc3ead3b604d1212c3c47a839ad0f2f94
Mark Sadegi <mark.sadegi@gmail.com>
2011-03-16 20:32:23 -0500

    blah

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,23 +1,38 @@
 // jQuery documents
 $(document).ready(function(){
-  fetchTweets();
+  fetchRepos();
+  // fetchTweets();
 });
 
 // JS Functions
 function fetchTweets(username){
   if (typeof username  == "undefined" || username == null) { username = "DrkStrife" };
   
-/*  $.ajax({
+  $.ajax({
       url: "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=" + username,
       type: "GET",
-      dataType: "json",
+      dataType: "text",
       success: function(dat){
         var data = $.parseJSON($(dat.responseText).text().trim().replace(new RegExp("\\n|\\r|\\t","g"),''));
         $("#tweets").html(data);
       }
     });
-*/        $.get("http://api.twitter.com/1/statuses/user_timeline.json?screen_name=" + username,function(data){
-          $("#tweets").html(data);
-          },"json");
+        // $.get("http://api.twitter.com/1/statuses/user_timeline.json?screen_name=" + username,function(data){
+ //          $("#tweets").html(data);
+ //          },"json");
 
+}
+
+function fetchRepos(username) {
+  if (typeof username == "undefined" || username == null) { username = "marka2g"};
+  
+  $.ajax({
+    url: "http://github.com/api/v1/json/" + username,
+    type: "GET",
+    dataType: "JSON",
+    success: function(dat){
+      var data = $.parseJSON(dat);
+      $("#repos").html(dat);
+    }
+  });
 }
