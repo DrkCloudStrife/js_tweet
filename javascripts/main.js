69e62212ee4b8c456385beb953dd6e2482a56e57
Eduardo Garibay <dark.cloud2007@gmail.com>
2011-03-16 19:53:56 -0500

    js get command test

--- javascripts/main.js
+++ javascripts/main.js
@@ -7,8 +7,17 @@ $(document).ready(function(){
 function fetchTweets(username){
   if (typeof username  == "undefined" || username == null) { username = "DrkStrife" };
   
-  $.getJSON("http://api.twitter.com/1/statuses/user_timeline.json?screen_name="+username,function(data){
-    $("#tweets").html(data)
-  });
+/*  $.ajax({
+      url: "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=" + username,
+      type: "GET",
+      dataType: "json",
+      success: function(dat){
+        var data = $.parseJSON($(dat.responseText).text().trim().replace(new RegExp("\\n|\\r|\\t","g"),''));
+        $("#tweets").html(data);
+      }
+    });
+*/        $.get("http://api.twitter.com/1/statuses/user_timeline.json?screen_name=" + username,function(data){
+          $("#tweets").html(data);
+          },"json");
 
 }
