d479eb478be79ec4e3f0f3408f3e0011405b31bb
Eduardo Garibay <dark.cloud2007@gmail.com>
2011-03-17 09:50:24 -0500

    fixing javascript code to return basic info for tweets and git repo urls

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,37 +1,27 @@
 // jQuery documents
 $(document).ready(function(){
-  // fetchRepos();
-  // fetchTweets();
+ fetchRepos();
+ fetchTweets();
 });
 
 // JS Functions
 function fetchTweets(username){
   if (typeof username  == "undefined" || username == "") { username = "DrkStrife" };
-  $.ajax({
-      url: "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=" + username,
-      type: "GET",
-      dataType: "JSON",
-      success: function(dat){
-        var data = $.parseJSON($(dat.responseText).text().trim().replace(new RegExp("\\n|\\r|\\t","g"),''));
-        $("#tweets").html(data);
-      }
-    });
-        // $.get("http://api.twitter.com/1/statuses/user_timeline.json?screen_name=" + username,function(data){
- //          $("#tweets").html(data);
- //          },"json");
+  
+  $.getJSON('http://api.twitter.com/1/statuses/user_timeline.json?screen_name='+ username+'&callback=?', function(json) { //get information about the user
+    for(var x in json){
+     $('#tweets').append("<p>"+json[x].text+"</p>"); 
+    }
+  });
 
 }
 
 function fetchRepos(username) {
   if (typeof username == "undefined" || username == "") { username = "marka2g"};
   
-  $.ajax({
-    url: "http://github.com/api/v1/json/" + username,
-    type: "GET",
-    dataType: "JSON",
-    success: function(dat){
-      var data = $.parseJSON(dat);
-      $("#repos").html(dat);
+  $.getJSON('http://github.com/api/v1/json/'+ username+'?callback=?', function(json) { //get information about the user
+    for(var x in json['user']['repositories']){
+     $('#repos').append("<p><a href=\""+json['user']['repositories'][x].url+"\">"+json['user']['repositories'][x].name+"</a></p>"); 
     }
   });
 }
